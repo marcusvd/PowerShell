19cbbc6435436573d608b6948c7f4ba1feb928d9
marcusvd <marcusmvd@yahoo.com.br>
2024-04-25 19:11:03 -0300

    bkp-single-disk-added

---
+++ Backup/SingleDisk/BackupModule.psm1
@@ -0,0 +1,124 @@
+
+#region SendMail
+Function SendMail([string]$subject, [string]$body) {
+
+    $Email = New-Object System.Net.Mail.MailMessage
+
+    $Email.subject = $subject
+    $Email.body = $body
+    $Email.to.add("backup@nostopti.com.br")
+    #$Email.CC.Add("lucaslaender@hotmail.com")
+    $Email.from = "backup@nostopti.com.br"
+    #$Email.attachments.add(Attachment)
+
+    $Smtp = New-Object System.Net.Mail.SmtpClient("smtp.nostopti.com.br", 587);
+    #$Smtp.EnableSsl =$true
+    $Smtp.Credentials = New-Object System.Net.NetworkCredential("backup@nostopti.com.br", "Nsti@2024");
+    $Smtp.Send($Email)
+   
+}
+Export-ModuleMember -Function SendMail
+#endregion
+
+#region VmsCheckExists
+#$Date = Get-Date -Format "dd_MM_yyyy"
+#$VmsPaths = @("C:\Util\BACKUP\$($Date)\SRVAD01\SRVAD01.VHDX", "C:\Util\BACKUP\$($Date)\SRVFL01\SRVFL01.vhdx", "C:\Util\BACKUP\$($Date)\SRVFL01\DADOS.vhdx", "C:\Util\BACKUP\$($Date)\SRVSH01\SRVSH01.vhdx")
+
+Function VmsCheckExists([string[]]$paths) { 
+
+$resultBoolFalse = @()
+
+$paths.ForEach({
+           if(!(Test-Path($_)))
+           {
+            $resultBoolFalse += [System.IO.File]::Exists($_)
+           }
+        })
+    
+    if($resultBoolFalse.Count -gt 0)
+    {
+     Write-Host("Fail")
+     #SendMail($false)
+     return $false
+    }
+
+   Write-Host("Success")
+   return $true
+   
+}
+#VmsCheckExists -paths $VmsPaths
+Export-ModuleMember -Function VmsCheckExists
+#endregion
+
+#region CheckFreeSpaceDisk
+Function CheckFreeSpaceDisk {
+    Param([Parameter(ValueFromPipeLine)]
+        [string]$DriverLetter
+    )
+
+    [int]$result = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$DriverLetter'"  | ForEach-Object { [Math]::Round(($_.FreeSpace / 1GB), 2) }
+
+    return $result
+}
+#CheckFreeSpaceDisk -DriverLetter "c:"
+Export-ModuleMember -Function CheckFreeSpaceDisk
+#endregion
+
+#region CalcFilesSize
+Function CalcFilesSize{
+Param([Parameter(ValueFromPipeline)]
+[string]$pathToFile, [string]$extension 
+)
+
+$pathFiles = Join-Path -Path $pathToFile -ChildPath $extension
+
+
+$getAllFfiles = Get-ChildItem -Path $pathFiles -Recurse
+
+$vhdxTotalSize = $null
+
+$getAllFfiles.ForEach({
+    $vhdxTotalSize += $_.Length
+})
+
+[int]$result = $vhdxTotalSize | ForEach-Object {[Math]::Round(($_ / 1GB),2)}
+
+return $result
+
+}
+#CalcFilesSize -pathToFile "\\srvhv10\v$\VMS\SERVIDORES" -extension "*.vhd*"
+Export-ModuleMember -Function CalcFilesSize
+#endregion
+
+#region BackUpCopy
+Function BackUpCopy {
+    Param([Parameter(ValueFromPipeLine)]
+        [string]$target, [string]$destiny
+    )
+
+    $Date = Get-Date -Format "dd_MM_yyyy"
+
+    $destinyPlace = Join-Path -Path $destiny -ChildPath $Date
+
+    write-host("-------BACKUPING...-------")
+    Write-Host($target, $destinyPlace)
+    Copy-Item -Path $target -Destination $destinyPlace -Recurse -Force
+}
+#BackUpCopy -target '\\SRVHV10\V$\vms\*' -destiny 'c:\users\adm02\desktop'
+Export-ModuleMember -Function BackUpCopy
+#endregion 
+
+#region DeleteOlderFiles
+Function DeleteOlderFiles {
+    Param([Parameter(ValueFromPipeLine)]
+        [string]$PathFilesToDelete, [int]$daysBack
+    )
+
+ $result = Get-ChildItem  -Path $PathFilesToDelete -Directory | where {$_.CreationTime -le $(get-date).AddDays(-$daysBack)} | Remove-Item -recurse  
+ $result
+  Write-host($result)
+    
+}
+Export-ModuleMember -Function DeleteOlderFiles
+#DeleteOlderFiles -PathFilesToDelete $DriveA
+#endregion
---
+++ Backup/SingleDisk/VMS.ps1
@@ -0,0 +1,59 @@
+Import-Module -Name "c:\Util\BackupModule.psm1" -Force
+
+
+$Date = Get-Date -Format "dd_MM_yyyy"
+$VmsPathsSource = @("\\srvhv01\v$\VMS\SRVAD01\SRVAD01.VHDX", "\\srvhv01\v$\VMS\SRVFL01\SRVFL01.vhdx", "\\srvhv01\v$\VMS\SRVFL01\DADOS.vhdx", "\\srvhv01\v$\VMS\SRVSH01\SRVSH01.vhdx")
+$VmsPathsDestiny = @("C:\Util\BACKUP\$($Date)\SRVAD01\SRVAD01.VHDX", "C:\Util\BACKUP\$($Date)\SRVFL01\SRVFL01.vhdx", "C:\Util\BACKUP\$($Date)\SRVFL01\DADOS.vhdx", "C:\Util\BACKUP\$($Date)\SRVSH01\SRVSH01.vhdx")
+$PathVmsToBackupCalculateAmount = "\\srvhv01\v$\VMS\"
+$ExtensionFilesToCalculateAmount = "*.vhd*"
+$DriveToBackupIsFreeSpace = "c:"
+$PathToBackupFiles = "c:\Util\BACKUP"
+$PathToDelete = "c:\Util\BACKUP\"
+$MsgSource = "VMS--(SOURCE-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
+$MsgDestiny = "VMS--(DESTINY-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
+$daysBack = 8 #total de dias que será preservado de backups
+
+Function TotalFilesToBackup{
+return CalcFilesSize -pathToFile $PathVmsToBackupCalculateAmount -extension $ExtensionFilesToCalculateAmount
+}
+
+Function TotalFreeSpaceLocalDriveToBackup{
+return CheckFreeSpaceDisk -DriverLetter $DriveToBackupIsFreeSpace
+}
+
+
+
+if(!(VmsCheckExists -paths $VmsPathsSource)){
+SendMail($MsgSource)
+#Shutdown
+Invoke-Command{shutdown -s -f -t 120}
+}
+
+
+Function CheckDestiny{
+if(!(VmsCheckExists -paths $VmsPathsDestiny)){
+SendMail($MsgDestiny)
+#Shutdown
+Invoke-Command{shutdown -s -f -t 120}
+}
+}
+
+
+
+[int]$DriveToBackup = TotalFreeSpaceLocalDriveToBackup
+[int]$FilesForBack = TotalFilesToBackup
+
+While($DriveToBackup -lt $FilesForBack){
+$daysBack--
+DeleteOlderFiles -PathFilesToDelete $PathToDelete -daysBack $daysBack
+$DriveToBackup = TotalFreeSpaceLocalDriveToBackup
+$FilesForBack = TotalFilesToBackup
+Write-host($daysBack)
+}
+
+
+BackUpCopy -target $PathVmsToBackupCalculateAmount -destiny $PathToBackupFiles
+
+CheckDestiny
+
+Invoke-Command{shutdown -s -f -t 120}
