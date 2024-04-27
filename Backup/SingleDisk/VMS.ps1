e87597a76e72ecffedbfb1d4a756f5d6f09f9cbf
marcusvd <marcusmvd@yahoo.com.br>
2024-04-27 19:42:39 -0300

    update-5

--- Backup/SingleDisk/VMS.ps1
+++ Backup/SingleDisk/VMS.ps1
@@ -1,49 +1,44 @@
-# Import-Module -Name "c:\Util\BackupModule.psm1" -Force
-# $Date = Get-Date -Format "dd_MM_yyyy"
-# $VmsPathsSource = @("\\srvhv01\v$\VMS\SRVAD01\SRVAD01.VHDX", "\\srvhv01\v$\VMS\SRVFL01\SRVFL01.vhdx", "\\srvhv01\v$\VMS\SRVFL01\DADOS.vhdx", "\\srvhv01\v$\VMS\SRVSH01\SRVSH01.vhdx")
-# $VmsPathsDestiny = @("C:\Util\BACKUP\$($Date)\SRVAD01\SRVAD01.VHDX", "C:\Util\BACKUP\$($Date)\SRVFL01\SRVFL01.vhdx", "C:\Util\BACKUP\$($Date)\SRVFL01\DADOS.vhdx", "C:\Util\BACKUP\$($Date)\SRVSH01\SRVSH01.vhdx")
-# $PathVmsToBackupCalculateAmount = "\\srvhv01\v$\VMS\"
-# $ExtensionFilesToCalculateAmount = "*.vhd*"
-# $DriveToBackupIsFreeSpace = "c:"
-# $PathToBackupFiles = "c:\Util\BACKUP"
-# $PathToDelete = "c:\Util\BACKUP\"
-# $MsgSource = "VMS--(SOURCE-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
-# $MsgDestiny = "VMS--(DESTINY-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
-# $daysBack = 8 #total de dias que ser� preservado de backups
-
-
-# Import-Module Import-Json
-
 
 $headerJson = Get-Content -Path "C:\Util\BackupHeader.json" -Raw
 
 $powerShellObj = $headerJson | ConvertFrom-Json
 
+Import-Module -Name $powerShellObj.Module -Force
+#$Date = $powerShellObj.Date
+$VmsPathsSource = $powerShellObj.VmsPathsSource
+$VmsPathsDestiny = $powerShellObj.VmsPathsDestiny
+$PathVmsToBackupCalculateAmount = $powerShellObj.PathVmsToBackupCalculateAmount
+$ExtensionFilesToCalculateAmount = $powerShellObj.ExtensionFilesToCalculateAmount
+$DriveToBackupIsFreeSpace = $powerShellObj.DriveToBackupIsFreeSpace
+$PathToBackupFiles = $powerShellObj.PathToBackupFiles
+$PathToDelete = $powerShellObj.PathToDelete
+$MsgSource = $powerShellObj.MsgSource
+$MsgDestiny = $powerShellObj.MsgDestiny
+$daysBack = $powerShellObj.daysBack
 
-write-host($powerShellObj.VmsPathsSource)
-Function TotalFilesToBackup{
-return CalcFilesSize -pathToFile $PathVmsToBackupCalculateAmount -extension $ExtensionFilesToCalculateAmount
+Function TotalFilesToBackup {
+    return CalcFilesSize -pathToFile $PathVmsToBackupCalculateAmount -extension $ExtensionFilesToCalculateAmount
 }
 
-Function TotalFreeSpaceLocalDriveToBackup{
-return CheckFreeSpaceDisk -DriverLetter $DriveToBackupIsFreeSpace
+Function TotalFreeSpaceLocalDriveToBackup {
+    return CheckFreeSpaceDisk -DriverLetter $DriveToBackupIsFreeSpace
 }
 
 
 
-if(!(VmsCheckExists -paths $VmsPathsSource)){
-SendMail($MsgSource)
-#Shutdown
-Invoke-Command{shutdown -s -f -t 120}
+if (!(VmsCheckExists -paths $VmsPathsSource)) {
+    SendMail($MsgSource)
+    #Shutdown
+    Invoke-Command { shutdown -s -f -t 120 }
 }
 
 
-Function CheckDestiny{
-if(!(VmsCheckExists -paths $VmsPathsDestiny)){
-SendMail($MsgDestiny)
-#Shutdown
-Invoke-Command{shutdown -s -f -t 120}
-}
+Function CheckDestiny {
+    if (!(VmsCheckExists -paths $VmsPathsDestiny)) {
+        SendMail($MsgDestiny)
+        #Shutdown
+        Invoke-Command { shutdown -s -f -t 120 }
+    }
 }
 
 
@@ -51,12 +46,12 @@ Invoke-Command{shutdown -s -f -t 120}
 [int]$DriveToBackup = TotalFreeSpaceLocalDriveToBackup
 [int]$FilesForBack = TotalFilesToBackup
 
-While($DriveToBackup -lt $FilesForBack){
-$daysBack--
-DeleteOlderFiles -PathFilesToDelete $PathToDelete -daysBack $daysBack
-$DriveToBackup = TotalFreeSpaceLocalDriveToBackup
-$FilesForBack = TotalFilesToBackup
-Write-host($daysBack)
+While ($DriveToBackup -lt $FilesForBack) {
+    $daysBack--
+    DeleteOlderFiles -PathFilesToDelete $PathToDelete -daysBack $daysBack
+    $DriveToBackup = TotalFreeSpaceLocalDriveToBackup
+    $FilesForBack = TotalFilesToBackup
+    Write-host($daysBack)
 }
 
 
@@ -64,5 +59,4 @@ BackUpCopy -target $PathVmsToBackupCalculateAmount -destiny $PathToBackupFiles
 
 CheckDestiny
 
-Invoke-Command{shutdown -s -f -t 120}
-Write-Host("Test of hash")
+Invoke-Command { shutdown -s -f -t 120 }
