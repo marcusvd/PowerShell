4d271a0f6c6a0188e460cb9471864de0b1b380a3
marcusvd <marcusmvd@yahoo.com.br>
2024-04-27 19:02:03 -0300

    update-4

--- Backup/SingleDisk/VMS.ps1
+++ Backup/SingleDisk/VMS.ps1
@@ -1,18 +1,26 @@
-Import-Module -Name "c:\Util\BackupModule.psm1" -Force
+# Import-Module -Name "c:\Util\BackupModule.psm1" -Force
+# $Date = Get-Date -Format "dd_MM_yyyy"
+# $VmsPathsSource = @("\\srvhv01\v$\VMS\SRVAD01\SRVAD01.VHDX", "\\srvhv01\v$\VMS\SRVFL01\SRVFL01.vhdx", "\\srvhv01\v$\VMS\SRVFL01\DADOS.vhdx", "\\srvhv01\v$\VMS\SRVSH01\SRVSH01.vhdx")
+# $VmsPathsDestiny = @("C:\Util\BACKUP\$($Date)\SRVAD01\SRVAD01.VHDX", "C:\Util\BACKUP\$($Date)\SRVFL01\SRVFL01.vhdx", "C:\Util\BACKUP\$($Date)\SRVFL01\DADOS.vhdx", "C:\Util\BACKUP\$($Date)\SRVSH01\SRVSH01.vhdx")
+# $PathVmsToBackupCalculateAmount = "\\srvhv01\v$\VMS\"
+# $ExtensionFilesToCalculateAmount = "*.vhd*"
+# $DriveToBackupIsFreeSpace = "c:"
+# $PathToBackupFiles = "c:\Util\BACKUP"
+# $PathToDelete = "c:\Util\BACKUP\"
+# $MsgSource = "VMS--(SOURCE-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
+# $MsgDestiny = "VMS--(DESTINY-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
+# $daysBack = 8 #total de dias que ser� preservado de backups
 
 
-$Date = Get-Date -Format "dd_MM_yyyy"
-$VmsPathsSource = @("\\srvhv01\v$\VMS\SRVAD01\SRVAD01.VHDX", "\\srvhv01\v$\VMS\SRVFL01\SRVFL01.vhdx", "\\srvhv01\v$\VMS\SRVFL01\DADOS.vhdx", "\\srvhv01\v$\VMS\SRVSH01\SRVSH01.vhdx")
-$VmsPathsDestiny = @("C:\Util\BACKUP\$($Date)\SRVAD01\SRVAD01.VHDX", "C:\Util\BACKUP\$($Date)\SRVFL01\SRVFL01.vhdx", "C:\Util\BACKUP\$($Date)\SRVFL01\DADOS.vhdx", "C:\Util\BACKUP\$($Date)\SRVSH01\SRVSH01.vhdx")
-$PathVmsToBackupCalculateAmount = "\\srvhv01\v$\VMS\"
-$ExtensionFilesToCalculateAmount = "*.vhd*"
-$DriveToBackupIsFreeSpace = "c:"
-$PathToBackupFiles = "c:\Util\BACKUP"
-$PathToDelete = "c:\Util\BACKUP\"
-$MsgSource = "VMS--(SOURCE-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
-$MsgDestiny = "VMS--(DESTINY-FILES)--(ARC-AR)-- Backup (FAIL)", "----NO STOP-TI----"
-$daysBack = 8 #total de dias que ser� preservado de backups
+# Import-Module Import-Json
 
+
+$headerJson = Get-Content -Path "C:\Util\BackupHeader.json" -Raw
+
+$powerShellObj = $headerJson | ConvertFrom-Json
+
+
+write-host($powerShellObj.VmsPathsSource)
 Function TotalFilesToBackup{
 return CalcFilesSize -pathToFile $PathVmsToBackupCalculateAmount -extension $ExtensionFilesToCalculateAmount
 }
