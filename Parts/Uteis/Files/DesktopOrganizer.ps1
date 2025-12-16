fcc4cdde8146f1fa1918fc04fc53c586c38520ac
marcusvd <marcusmvd@gmail.com>
2025-12-16 13:01:05 -0300

    desktop_organizer

--- Parts/Uteis/Files/DesktopOrganizer.ps1
+++ Parts/Uteis/Files/DesktopOrganizer.ps1
@@ -1,5 +1,4 @@
 
-
 class Org {
     [string]$PathProFileUser
     [string[]]$AllItems
@@ -8,16 +7,40 @@ class Org {
 }
 
 $Org = [Org]::new()
-$Org.PathProFileUser = "$env:USERPROFILE\desktop\"
+$Org.PathProFileUser = [Environment]::GetFolderPath('Desktop')
+
 $Org.AllItems = Get-ChildItem -Path $Org.PathProFileUser
 
 foreach ($names in $Org.AllItems) {
     $Org.Extensions += [System.IO.Path]::GetExtension($names)
 }
 
+$Org.Extensions = $Org.Extensions | Where-Object { $_ -ne "" }
+
+$foldersPath = [System.Collections.Generic.List[string]]::new()
+
 foreach ($Extensions in $Org.Extensions = $Org.Extensions | Sort-Object -Unique) {
-   New-Item -ItemType Directory -Path $Org.PathProFileUser -Name $Extensions -ErrorAction SilentlyContinue
-   $src = $Org.PathProFileUser+'*'+$Extensions
-   $dty = $Org.PathProFileUser+$Extensions
-   Move-Item -Path $src -Destination $dty  -ErrorAction SilentlyContinue 
+    New-Item -ItemType Directory -Path $Org.PathProFileUser -Name $Extensions.Replace(".", "") -ErrorAction SilentlyContinue;
+
+    $ddd = Join-Path -Path $Org.PathProFileUser -ChildPath  ($Extensions.Replace(".", ""));
+
+    $foldersPath.Add($ddd)
 }
+
+foreach ($item in $foldersPath) {
+    $lastPart = [System.IO.Path]::GetFileName($item)
+    $lastPart
+
+    foreach ($filesExtension in $Org.Extensions) {
+        
+      $file =  $filesExtension.Replace(".", "");
+
+        if ($file -eq $lastPart) {
+            $src = $item.Replace($file, "") + '*.' + $file
+            Move-Item -Path $src -Destination $item  -ErrorAction SilentlyContinue 
+        }
+    }
+
+   # Move-Item -Path $src -Destination $dty  -ErrorAction SilentlyContinue 
+}
+  
